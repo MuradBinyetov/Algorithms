b7a022576d931e7d1a6cb3da6b073b247b6a2db0
Murad Binyetov <muradbinyetov@gmail.com>
2025-07-22 07:55:27 +0400

    2 algorithms completed

---
+++ algorithm/src/main/java/solutions/leetcode/remove_Linked_List_Elements.java
@@ -0,0 +1,14 @@
+package solutions.leetcode;
+
+public class remove_Linked_List_Elements {
+    public static ListNode removeElements(ListNode head, int val) {
+        if (head == null) return null;
+        head.next = removeElements(head.next, val);
+        if(head.val == val){
+            return head.next;
+        }
+        else{
+            return head;
+        }
+    }
+}
