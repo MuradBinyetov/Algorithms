081d616a4e4418a632e10e47a5cd7492e83a5877
Murad Binyetov <muradbinyetov@gmail.com>
2025-07-30 08:13:40 +0400

    1 algorithms completed

---
+++ algorithm/src/main/java/solutions/leetcode/summary_Ranges.java
@@ -0,0 +1,28 @@
+package solutions.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class summary_Ranges {
+    public static List<String> summaryRanges(int[] nums) { //[0,2,3,4,6,8,9]
+        List<String> result = new ArrayList<>();
+        if (nums == null || nums.length == 0) return result;
+
+        int start = nums[0];
+
+        for (int i = 1; i <= nums.length; i++) {
+            if (i == nums.length || nums[i] != nums[i - 1] + 1) {
+                if (start == nums[i - 1]) {
+                    result.add(String.valueOf(start));
+                } else {
+                    result.add(start + "->" + nums[i - 1]);
+                }
+                if (i < nums.length) {
+                    start = nums[i];
+                }
+            }
+        }
+
+        return result;
+    }
+}
