163396d32929e1118ce202d42d69abddcad59e66
Murad Binyatov <murad_binyetov@mail.ru>
2025-08-25 10:36:02 +0400

    1 algorithm completed

---
+++ algorithm/src/main/java/solutions/leetcode/first_Unique_Character_In_String.java
@@ -0,0 +1,19 @@
+﻿package solutions.leetcode;
+
+import java.util.HashMap;
+
+public class first_Unique_Character_In_String {
+    public int firstUniqChar(String s) {
+        var dict = new HashMap<Character, Integer>();
+
+        for (int i = 0; i < s.length(); i++) {
+            dict.put(s.charAt(i), dict.getOrDefault(s.charAt(i), 0) + 1);
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (dict.get(s.charAt(i)) == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
